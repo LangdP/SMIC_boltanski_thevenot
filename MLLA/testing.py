fc95e12c9ab5a13c4956a2c4a6f95d0b2786d803
Quentin Dénigot <q.denigot@gmail.com>
2021-05-28 10:29:08 +0200

    starting model version 2

--- MLLA/testing.py
+++ MLLA/testing.py
@@ -1,4 +1,4 @@
-#Import packages
+# Import packages
 from BTmodel_v1 import *
 from viz_functions import *
 
@@ -16,16 +16,16 @@ industrial_speaker = Speaker("industrial", 1)
 industrial_listener = Listener("industrial", 1)
 
 props = ["competent", "incompetent"]
-indus_speaker_pred = {m : industrial_speaker.choice_rule(m, messages, props[0]) 
-    for m in messages}
+indus_speaker_pred = {m: industrial_speaker.choice_rule(m, messages, props[0])
+                      for m in messages}
 
 # We can use the viz function for detailed look at one case
 viz(indus_speaker_pred)
 
-indus_speaker_pred = {p : 
-    {m : industrial_speaker.choice_rule(m, messages, p) 
-        for m in messages} 
-    for p in props}
+indus_speaker_pred = {p:
+                      {m: industrial_speaker.choice_rule(m, messages, p)
+                       for m in messages}
+                      for p in props}
 
 # We can also use it to generate barplot with many cases
 viz(indus_speaker_pred)
@@ -34,18 +34,18 @@ civic_speaker = Speaker("civic", 1)
 civic_listener = Listener("civic", 1)
 
 props = ["friendly", "unfriendly"]
-civic_speaker_pred = {p : 
-    {m : civic_speaker.choice_rule(m, messages, p) 
-        for m in messages} 
-    for p in props}
+civic_speaker_pred = {p:
+                      {m: civic_speaker.choice_rule(m, messages, p)
+                       for m in messages}
+                      for p in props}
 viz(civic_speaker_pred)
 
 inspired_speaker = Speaker("inspired", 1)
 inspired_listener = Listener("inspired", 1)
 
 props = ["insightful", "uninsightful"]
-inspired_speaker_pred = {p : 
-    {m : inspired_speaker.choice_rule(m, messages, p) 
-        for m in messages} 
-    for p in props}
+inspired_speaker_pred = {p:
+                         {m: inspired_speaker.choice_rule(m, messages, p)
+                          for m in messages}
+                         for p in props}
 viz(inspired_speaker_pred)
--- MLLA/viz_functions.py
+++ MLLA/viz_functions.py
@@ -6,6 +6,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+
 def viz(outcomes, lis=False):
     # This function takes in a series of states and their probabilities
     # in the form of a dictionary and gives out a histogram representing them.
@@ -16,7 +17,7 @@ def viz(outcomes, lis=False):
     else:
         xlab = "Utterances"
         title = "Speaker intentions"
-    
+
     full_results = True
     for key in outcomes:
         if type(outcomes[key]) == dict:
@@ -24,21 +25,21 @@ def viz(outcomes, lis=False):
         else:
             full_results = False
 
-    if full_results: 
-        dfs = {key:pd.DataFrame({'x':list(outcomes[key].keys()), 
-                'y': list(outcomes[key].values())}) for key in outcomes}
+    if full_results:
+        dfs = {key: pd.DataFrame({'x': list(outcomes[key].keys()),
+                                 'y': list(outcomes[key].values())}) for key in outcomes}
         df_list = []
         for df_key in dfs.keys():
             dfs[df_key]['hue'] = df_key
             df_list.append(dfs[df_key])
-        
-        res=pd.concat(df_list)
-        ax = sns.barplot(x = 'x', y = 'y', 
-            data = res, 
-            hue = 'hue')
-        ax.set(xlabel = xlab, ylabel = ylab, title = title)
+
+        res = pd.concat(df_list)
+        ax = sns.barplot(x='x', y='y',
+                         data=res,
+                         hue='hue')
+        ax.set(xlabel=xlab, ylabel=ylab, title=title)
         ax.legend(bbox_to_anchor=(1.01, 1),
-           borderaxespad=0, )
+                  borderaxespad=0, )
         plt.tight_layout()
         return plt.show()
     else:
